66a189f4f00df4838a2f08a80ee586b1b8fecb9d
Robert Peralta <robert.peralta@helloglobo.com>
2018-04-02 19:45:13 +0200

    Revert

--- Formula/drush.rb
+++ Formula/drush.rb
@@ -18,6 +18,7 @@ class Drush < Formula
   end
 
   depends_on PhpMetaRequirement
+  depends_on "php55" if Formula["php55"].linked_keg.exist?
   depends_on "php56" if Formula["php56"].linked_keg.exist?
 
   def install
